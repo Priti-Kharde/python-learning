c3d468703e211abbe342b71d9e1cc0003e2cb371
Priti-Kharde <pritikharde5@gmail.com>
2025-06-20 23:34:28 +0530

    Add files via upload
    stringConcat, userInput and int() function files added

---
+++ int().py
@@ -0,0 +1,34 @@
+# num1 = input("Enter first number ")
+# num2 = input("Enter second number ")
+# sum = num1 + num2
+# print(sum)
+#above code will concantenate the numbers as input() consider them as strings.
+
+
+# num1 = int(input("Enter first number "))
+# num2 = int(input("Enter second number "))
+# sum = num1 + num2
+# print(sum)
+#above code will sum up the numbers entered by the user i.e 4+5 =9
+
+
+# num1 = int(input("Enter first number "))
+# num2 = int(input("Enter second number "))
+# sum = num1 + num2
+# print(str(sum))
+# THIS IS EXPECTED OUTPUT
+
+
+
+
+# num1 = int(input("Enter first number "))
+# num2 = int(input("Enter second number "))
+# sum = num1 + num2
+# print("total is " + sum)
+# above code will also give error because we are adding string means "total is" with sum,because it is a number as we used int function to get input from user in int form
+
+
+num1 = str(5)
+num2 = float("10")
+num3 = int("33")
+print(num2 + num3 )
